7001db6b85e49918dc8d39d718e61fac5479e009
IndyBob2019 <davidrichardson@Davids-MacBook-Pro.local>
2016-08-15 21:47:46 -0400

    Added Project

---
+++ HomeworkHelper/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HomeworkHelper
+//
+//  Created by David Richardson on 5/20/16.
+//  Copyright © 2016 David Richardson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
